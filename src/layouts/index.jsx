3d5afecaf9b4befcd2340677d73b71440dd197e8
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-06-27 22:44:08 +0530

    fix: sidebar in layout

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,11 @@
+import Sidebar from "@/components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 function DashboardLayout() {
   return (
     <div className="flex h-screen">
       <div className="hidden w-72 flex-col bg-muted md:flex">
-        {/* Sidebar here */}
+        <Sidebar />
       </div>
       <main className="flex flex-1 flex-col max-w-screen-2xl mx-auto overflow-y-auto p-6 md:p-8">
         <Outlet />
