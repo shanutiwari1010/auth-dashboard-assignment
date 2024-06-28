5208de19351b420a26b1911fe7220b5f60b65983
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-06-28 10:26:08 +0530

    feat: added header,dataTable to dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Header from "@/components/Header";
+import DataTable from "@/components/DataTable";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import { getUsersList } from "@/api/users";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const { data, isLoading } = useQuery({
+    queryFn: () => getUsersList(),
+    queryKey: ["users-list"],
+    refetchOnWindowFocus: true,
+  });
 
-export default Dashboard
+  return (
+    <div className="flex-1 md:flex h-screen relative">
+      <Header />
+      <main className="bg-gray-50 py-6 px-9 mt-12 w-full h-fit flex flex-col gap-10">
+        <section>
+          <DataTable data={data?.data} loading={isLoading} />
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Dashboard;
