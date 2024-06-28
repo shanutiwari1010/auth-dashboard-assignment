38de06428a8a150ca59988c9a7281d3f34af59ba
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-06-28 08:18:05 +0530

    feat: added user list and profile update api

---
+++ src/api/users.js
@@ -0,0 +1,7 @@
+export const getUsersList = async () => {
+  return await axios.get("/userList");
+};
+
+export const updateUserProfile = async (data) => {
+  return await axios.post("/profileUpdate", data);
+};
