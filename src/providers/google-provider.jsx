278c521dbe526e648d86bc1fc48e6d616929796a
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-07-02 12:39:31 +0530

    linting

--- src/providers/google-provider.jsx
+++ src/providers/google-provider.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { AUTH_GOOGLE_CLIENT_ID } from "@/config/vars";
 
+// eslint-disable-next-line react/prop-types
 const GoogleAuthProvider = ({ children }) => {
   return (
     <GoogleOAuthProvider clientId="455334685417-3t7gi7l3h2jhj70tr0hbfrf57sb1k3he.apps.googleusercontent.com">
