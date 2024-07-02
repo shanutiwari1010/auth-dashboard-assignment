aab4f5266704844bf38140f96906a32d3a0fffb2
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-07-02 12:44:12 +0530

    refactor/fix: eslint and google provider

--- src/main.jsx
+++ src/main.jsx
@@ -14,10 +14,12 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <App />
-        <Toaster />
-      </Provider>
+      <GoogleAuthProvider>
+        <Provider store={store}>
+          <App />
+          <Toaster />
+        </Provider>
+      </GoogleAuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
