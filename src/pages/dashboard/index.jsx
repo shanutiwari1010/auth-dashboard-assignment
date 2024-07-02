97336ad8e7d56c4034d0c38710624a88d10663cc
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-07-02 12:31:35 +0530

    fix/refactor: query and state changes

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Header from "@/components/Header";
 import DataTable from "@/components/DataTable";
-import { useQuery } from "@tanstack/react-query";
 import { getUsersList } from "@/api/users";
 
 const Dashboard = () => {
-  const { data, isLoading } = useQuery({
-    queryFn: () => getUsersList(),
-    queryKey: ["users-list"],
-    refetchOnWindowFocus: true,
-  });
+  const [tableData, setTableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await getUsersList();
+        const data = response?.data;
+        setTableData(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, []);
 
   return (
     <div className="flex-1 md:flex h-screen relative">
       <Header />
       <main className="bg-gray-50 py-6 px-9 mt-12 w-full h-fit flex flex-col gap-10">
         <section>
-          <DataTable data={data?.data} loading={isLoading} />
+          <DataTable data={tableData} loading={isLoading} />
         </section>
       </main>
     </div>
